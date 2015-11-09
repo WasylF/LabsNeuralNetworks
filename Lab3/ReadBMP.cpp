a07eff62b8d75882ee055a128d7987371aef6488
WslF <WasylF@gmail.com>
2015-11-09 02:30:32 +0200

    refactor ReadBMP class

--- Lab3/ReadBMP.cpp
+++ Lab3/ReadBMP.cpp
@@ -1,182 +1,160 @@
 //solution by Wsl_F
-#include <bits/stdc++.h>
 #include "ReadBMP.h"
 
 using namespace std;
-#pragma comment(linker, "/STACK:1024000000,1024000000")
 
-
-typedef long long LL;
-typedef unsigned long long uLL;
-typedef double dbl;
-typedef vector<int> vi;
-typedef vector<LL> vL;
-typedef vector<string> vs;
-typedef pair<int,int> pii;
-typedef pair<LL,LL> pLL;
-
-#define mp(x,y)  make_pair((x),(y))
-#define pb(x)  push_back(x)
-#define sqr(x) ((x)*(x))
-class ReadBMP
+MatrixRGB ReadBMP:: GetRGBMatrix(string fileName, BITMAPINFOHEADER &bmiHeader)
 {
-    typedef vector<vector<RGBQUAD>> MatrixRGB;
+    FILE* pFile = fopen(fileName.c_str(), "rb");
 
-public:
-    static MatrixRGB GetRGBMatrix(string fileName, BITMAPINFOHEADER &bmiHeader)
+    // считываем заголовок файла
+    BITMAPFILEHEADER header __attribute__((unused));
+    readFileHeader(header, pFile);
+
+    // считываем заголовок изображения
+    readBMPHeader(bmiHeader, pFile);
+
+    // считываем непосредственно само изображение
+    MatrixRGB rgb= readPicture(bmiHeader, pFile);
+    fclose(pFile);
+    return rgb;
+}
+
+vi ReadBMP:: readBlackWhiteBMP(string fileName)
+{
+    BITMAPINFOHEADER bmiHeader;
+    MatrixRGB rgbRotate= GetRGBMatrix(fileName, bmiHeader);
+
+    MatrixRGB rgb= RotatePicture(rgbRotate, bmiHeader.biWidth, bmiHeader.biHeight);
+
+    vi ans= convertColorMatrixToBWvector(rgb,bmiHeader.biWidth,bmiHeader.biHeight);
+    // выводим результат
+    printBlackWhite(ans, bmiHeader.biWidth,  bmiHeader.biHeight);
+
+    return ans;
+}
+
+void ReadBMP:: printBlackWhite(vi ans, int width, int height)
+{
+    int k= 0;
+    for (int i= 0; i<height; i++)
     {
-        FILE* pFile = fopen(fileName.c_str(), "rb");
-
-        // считываем заголовок файла
-        BITMAPFILEHEADER header __attribute__((unused));
-        readFileHeader(header, pFile);
-
-        // считываем заголовок изображения
-        readBMPHeader(bmiHeader, pFile);
-
-        // считываем непосредственно само изображение
-        MatrixRGB rgb= readPicture(bmiHeader, pFile);
-        fclose(pFile);
-        return rgb;
+        for (int j= 0; j<width; j++)
+            if (ans[k++]) cout<<(char)178;
+            else cout<<' ';
+        cout<<endl;
     }
+}
 
-    static vi readBlackWhiteBMP(string fileName)
+vi ReadBMP:: convertColorMatrixToBWvector(MatrixRGB rgb, int width, int height)
+{
+    vi ans;
+    ans.clear();
+    for (int i = 0; i < height; i++)
+        for (int j = 0; j < width; j++)
+            if (rgb[i][j].rgbRed + rgb[i][j].rgbGreen + rgb[i][j].rgbBlue > 255*1.5)  ans.pb(0);
+            else ans.pb(1);
+    return ans;
+}
+
+MatrixRGB ReadBMP:: RotatePicture(MatrixRGB rgbRotated, int width, int height)
+{
+    MatrixRGB rgb(height);
+    vector<RGBQUAD> tmp;
+    tmp.clear();
+    for (int i = 0; i < width; i++)
+        for (int j = 0; j < height; j++)
+            tmp.pb(rgbRotated[i][j]);
+
+    for (int i= height-1; i>=0; i--)
+        reverse(tmp.begin()+width*(i),tmp.begin()+width*(i+1));
+    reverse(tmp.begin(),tmp.end());
+
+    for (int i= 0; i<height; i++)
+        for (int j= 0; j<width; j++)
+            rgb[i].pb(tmp[width*i+j]);
+
+    return rgb;
+}
+
+void ReadBMP:: readFileHeader(BITMAPFILEHEADER &header, FILE* pFile)
+{
+    header.bfType      = read_u16(pFile);
+    header.bfSize      = read_u32(pFile);
+    header.bfReserved1 = read_u16(pFile);
+    header.bfReserved2 = read_u16(pFile);
+    header.bfOffBits   = read_u32(pFile);
+}
+
+void ReadBMP:: readBMPHeader(BITMAPINFOHEADER &bmiHeader, FILE* pFile)
+{
+    bmiHeader.biSize          = read_u32(pFile);
+    bmiHeader.biWidth         = read_s32(pFile);
+    bmiHeader.biHeight        = read_s32(pFile);
+    bmiHeader.biPlanes        = read_u16(pFile);
+    bmiHeader.biBitCount      = read_u16(pFile);
+    bmiHeader.biCompression   = read_u32(pFile);
+    bmiHeader.biSizeImage     = read_u32(pFile);
+    bmiHeader.biXPelsPerMeter = read_s32(pFile);
+    bmiHeader.biYPelsPerMeter = read_s32(pFile);
+    bmiHeader.biClrUsed       = read_u32(pFile);
+    bmiHeader.biClrImportant  = read_u32(pFile);
+}
+
+MatrixRGB ReadBMP:: readPicture(BITMAPINFOHEADER bmiHeader, FILE* pFile)
+{
+    MatrixRGB rgb(bmiHeader.biWidth);
+
+    RGBQUAD tmp;
+    for (int i = 0; i < bmiHeader.biWidth; i++)
     {
-        BITMAPINFOHEADER bmiHeader;
-        MatrixRGB rgbRotate= GetRGBMatrix(fileName, bmiHeader);
-
-        MatrixRGB rgb= RotatePicture(rgbRotate, bmiHeader.biWidth, bmiHeader.biHeight);
-
-        vi ans= convertColorMatrixToBWvector(rgb,bmiHeader.biWidth,bmiHeader.biHeight);
-        // выводим результат
-        printBlackWhite(ans, bmiHeader.biWidth,  bmiHeader.biHeight);
-
-        return ans;
-    }
-
-    static void printBlackWhite(vi ans, int width, int height)
-    {
-        int k= 0;
-        for (int i= 0; i<height; i++)
+        for (int j = 0; j < bmiHeader.biHeight; j++)
         {
-            for (int j= 0; j<width; j++)
-                if (ans[k++]) cout<<(char)178;
-                else cout<<' ';
-            cout<<endl;
+            tmp.rgbBlue = getc(pFile);
+            tmp.rgbGreen = getc(pFile);
+            tmp.rgbRed = getc(pFile);
+            rgb[i].pb(tmp);
         }
+
+        // пропускаем последний байт в строке
+        getc(pFile);
     }
-private:
-    static vi convertColorMatrixToBWvector(MatrixRGB rgb, int width, int height)
-    {
-        vi ans;
-        ans.clear();
-        for (int i = 0; i < height; i++)
-            for (int j = 0; j < width; j++)
-                if (rgb[i][j].rgbRed + rgb[i][j].rgbGreen + rgb[i][j].rgbBlue > 255*1.5)  ans.pb(0);
-                else ans.pb(1);
-        return ans;
-    }
+    return rgb;
+}
 
-    static MatrixRGB RotatePicture(MatrixRGB rgbRotated, int width, int height)
-    {
-        MatrixRGB rgb(height);
-        vector<RGBQUAD> tmp;
-        tmp.clear();
-        for (int i = 0; i < width; i++)
-            for (int j = 0; j < height; j++)
-                tmp.pb(rgbRotated[i][j]);
+unsigned short ReadBMP:: read_u16(FILE *fp)
+{
+    unsigned char b0, b1;
 
-        for (int i= height-1; i>=0; i--)
-            reverse(tmp.begin()+width*(i),tmp.begin()+width*(i+1));
-        reverse(tmp.begin(),tmp.end());
+    b0 = getc(fp);
+    b1 = getc(fp);
 
-        for (int i= 0; i<height; i++)
-            for (int j= 0; j<width; j++)
-                rgb[i].pb(tmp[width*i+j]);
+    return ((b1 << 8) | b0);
+}
 
-        return rgb;
-    }
+unsigned int ReadBMP:: read_u32(FILE *fp)
+{
+    unsigned char b0, b1, b2, b3;
 
-    static void readFileHeader(BITMAPFILEHEADER &header, FILE* pFile)
-    {
-        header.bfType      = read_u16(pFile);
-        header.bfSize      = read_u32(pFile);
-        header.bfReserved1 = read_u16(pFile);
-        header.bfReserved2 = read_u16(pFile);
-        header.bfOffBits   = read_u32(pFile);
-    }
+    b0 = getc(fp);
+    b1 = getc(fp);
+    b2 = getc(fp);
+    b3 = getc(fp);
 
-    static void readBMPHeader(BITMAPINFOHEADER &bmiHeader, FILE* pFile)
-    {
-        bmiHeader.biSize          = read_u32(pFile);
-        bmiHeader.biWidth         = read_s32(pFile);
-        bmiHeader.biHeight        = read_s32(pFile);
-        bmiHeader.biPlanes        = read_u16(pFile);
-        bmiHeader.biBitCount      = read_u16(pFile);
-        bmiHeader.biCompression   = read_u32(pFile);
-        bmiHeader.biSizeImage     = read_u32(pFile);
-        bmiHeader.biXPelsPerMeter = read_s32(pFile);
-        bmiHeader.biYPelsPerMeter = read_s32(pFile);
-        bmiHeader.biClrUsed       = read_u32(pFile);
-        bmiHeader.biClrImportant  = read_u32(pFile);
-    }
+    return ((((((b3 << 8) | b2) << 8) | b1) << 8) | b0);
+}
 
-    static MatrixRGB readPicture(BITMAPINFOHEADER bmiHeader, FILE* pFile)
-    {
-        MatrixRGB rgb(bmiHeader.biWidth);
+int ReadBMP:: read_s32(FILE *fp)
+{
+    unsigned char b0, b1, b2, b3;
 
-        RGBQUAD tmp;
-        for (int i = 0; i < bmiHeader.biWidth; i++)
-        {
-            for (int j = 0; j < bmiHeader.biHeight; j++)
-            {
-                tmp.rgbBlue = getc(pFile);
-                tmp.rgbGreen = getc(pFile);
-                tmp.rgbRed = getc(pFile);
-                rgb[i].pb(tmp);
-            }
+    b0 = getc(fp);
+    b1 = getc(fp);
+    b2 = getc(fp);
+    b3 = getc(fp);
 
-            // пропускаем последний байт в строке
-            getc(pFile);
-        }
-        return rgb;
-    }
-
-    static unsigned short read_u16(FILE *fp)
-    {
-        unsigned char b0, b1;
-
-        b0 = getc(fp);
-        b1 = getc(fp);
-
-        return ((b1 << 8) | b0);
-    }
-
-    static unsigned int read_u32(FILE *fp)
-    {
-        unsigned char b0, b1, b2, b3;
-
-        b0 = getc(fp);
-        b1 = getc(fp);
-        b2 = getc(fp);
-        b3 = getc(fp);
-
-        return ((((((b3 << 8) | b2) << 8) | b1) << 8) | b0);
-    }
-
-    static int read_s32(FILE *fp)
-    {
-        unsigned char b0, b1, b2, b3;
-
-        b0 = getc(fp);
-        b1 = getc(fp);
-        b2 = getc(fp);
-        b3 = getc(fp);
-
-        return ((int)(((((b3 << 8) | b2) << 8) | b1) << 8) | b0);
-    }
-
-};
+    return ((int)(((((b3 << 8) | b2) << 8) | b1) << 8) | b0);
+}
 
 
 int main()
--- Lab3/ReadBMP.h
+++ Lab3/ReadBMP.h
@@ -1,3 +1,8 @@
+//solution by Wsl_F
+#include <bits/stdc++.h>
+
+using namespace std;
+
 typedef struct
 {
     unsigned int    bfType;
@@ -30,7 +35,27 @@ typedef struct
     int   rgbReserved;
 } RGBQUAD;
 
+typedef vector<int> vi;
+typedef vector<vector<RGBQUAD>> MatrixRGB;
 
-static unsigned short read_u16(FILE *fp);
-static unsigned int   read_u32(FILE *fp);
-static int            read_s32(FILE *fp);
+#define pb(x)  push_back(x)
+
+class ReadBMP
+{
+public:
+    static vi readBlackWhiteBMP(string fileName);
+
+private:
+    static MatrixRGB GetRGBMatrix(string fileName, BITMAPINFOHEADER &bmiHeader);
+    static void printBlackWhite(vi ans, int width, int height);
+    static vi convertColorMatrixToBWvector(MatrixRGB rgb, int width, int height);
+    static MatrixRGB RotatePicture(MatrixRGB rgbRotated, int width, int height);
+    static void readFileHeader(BITMAPFILEHEADER &header, FILE* pFile);
+    static void readBMPHeader(BITMAPINFOHEADER &bmiHeader, FILE* pFile);
+    static MatrixRGB readPicture(BITMAPINFOHEADER bmiHeader, FILE* pFile);
+
+    static unsigned short read_u16(FILE *fp);
+    static unsigned int   read_u32(FILE *fp);
+    static int            read_s32(FILE *fp);
+
+};
