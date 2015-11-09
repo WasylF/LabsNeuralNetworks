ca0cf3f138dae2990cf4f5c7e86ce8198432bc1e
WslF <WasylF@gmail.com>
2015-11-09 18:26:10 +0200

    Create solver - Lab3.cpp

---
+++ Lab3/Lab3.cpp
@@ -0,0 +1,22 @@
+//solution by Wsl_F
+#include <bits/stdc++.h>
+#include "ReadBMP.cpp"
+
+using namespace std;
+
+
+typedef vector<int> vi;
+typedef double dbl;
+#define pb(x)  push_back(x)
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout<<fixed;
+    cout<<setprecision(9);
+
+    return 0;
+}
